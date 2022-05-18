e640c2415218a75339d50331fc5f25504e328918
bbruscatto <bruscatto@gmail.com>
2022-05-18 18:22:10 -0300

    feat/yup&hookform
    Adding yup for verification and hook form to collect user data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
-import { Button, Flex, Stack, } from '@chakra-ui/react'
-import { Input } from '../components/Form/Input'
+import { Button, Flex, Stack, } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Input } from '../components/Form/Input';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup'
+
+type SignInFormatData = {
+  email: string;
+  password: string;
+
+}
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatório'),
+
+})
 
 export default function Home() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  })
+
+  const { errors } = formState
+
+  const handleSignIn: SubmitHandler<SignInFormatData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    console.log(values)
+  }
+
   return (
     <Flex
       w="100vw"
@@ -17,12 +44,29 @@ export default function Home() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" label="E-mail" />
-          <Input name="password" type="password" label="Password" />
+          <Input
+            name="email"
+            type="email"
+            label="E-mail"
+            error={errors.email}
+            {...register('email')}
+          />
+          <Input
+            name="password"
+            type="password"
+            label="Password"
+            {...register('password')}
+            error={errors.password}
+          />
         </Stack>
-        <Button type="submit" mt="6" colorScheme="pink">Entrar</Button>
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          isLoading={formState.isSubmitting}>Entrar</Button>
       </Flex>
     </Flex>
   )
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,16 @@
-import { Box, Button, Checkbox, Text, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Text, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true,
+    })
+
     return (
         <Box>
             <Header />
@@ -13,30 +19,32 @@ export default function UserList() {
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
                         <Heading size="lg" fontWeight="normal">Usuários</Heading>
-                        <Button
-                            as="a"
-                            size="sm"
-                            fontSize="sm"
-                            colorScheme="pink"
-                            leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-                        >
-                            Criar novo
-                        </Button>
+                        <Link href="/users/create" passHref>
+                            <Button
+                                as="a"
+                                size="sm"
+                                fontSize="sm"
+                                colorScheme="pink"
+                                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+                            >
+                                Criar novo
+                            </Button>
+                        </Link>
                     </Flex>
                     <Table colorScheme="whiteAlpha">
                         <Thead>
                             <Tr>
-                                <Th px="6" color="gray.300" w="8">
+                                <Th px={["4", "4", "6"]} color="gray.300" w="8">
                                     <Checkbox colorScheme="pink" />
                                 </Th>
                                 <Th>Usuários</Th>
-                                <Th>Data de cadastro</Th>
+                                {isWideVersion && <Th>Data de cadastro</Th>}
                                 <Th w="8"></Th>
                             </Tr>
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td px="6">
+                                <Td px={["4", "4", "6"]}>
                                     <Checkbox colorScheme="pink" />
                                 </Td>
                                 <Td>
@@ -45,7 +53,7 @@ export default function UserList() {
                                         <Text fontSize="sm" color="gray.500">bruscatto@gmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>11 de Maio, 2022</Td>
+                                {isWideVersion && <Td>11 de Maio, 2022</Td>}
                                 <Td>
                                     <Button
                                         as="a"
@@ -68,7 +76,7 @@ export default function UserList() {
                                         <Text fontSize="sm" color="gray.500">bruscatto@gmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>11 de Maio, 2022</Td>
+                                {isWideVersion && <Td>11 de Maio, 2022</Td>}
                                 <Td>
                                     <Button
                                         as="a"
@@ -91,7 +99,7 @@ export default function UserList() {
                                         <Text fontSize="sm" color="gray.500">bruscatto@gmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>11 de Maio, 2022</Td>
+                                {isWideVersion && <Td>11 de Maio, 2022</Td>}
                                 <Td>
                                     <Button
                                         as="a"
